cf93ed4f9488b391565220ba557c04f4730a6c81
bodrovis <bodrovis@protonmail.com>
2025-04-12 19:12:51 +0300

    bump

---
+++ src/api.ts
@@ -0,0 +1,23 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { LokaliseApi, LokaliseApiOAuth  } from "@lokalise/node-api";
+
+const apiKey = process.env.LOKALISE_API_TOKEN as string;
+
+async function main() {
+  // const lokaliseApi = new LokaliseApi({ apiKey });
+
+  // const project = await lokaliseApi.projects().get('');
+  // console.log(project);
+
+  const lokaliseApiOauth = new LokaliseApiOAuth({ apiKey: '' });
+
+  const projects = await lokaliseApiOauth.projects().list();
+  console.log(projects);
+}
+
+// Run the script
+main().catch((err) => {
+	console.error("Unexpected error:", err);
+});
